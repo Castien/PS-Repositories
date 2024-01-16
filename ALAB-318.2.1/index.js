950598e84bbda1e87859bb6e96e8aa80a809ac78
Eileen Lowers <funmaster22@hotmail.com>
2024-01-16 08:23:57 -0500

    three views in EJS

--- ALAB-318.2.1/index.js
+++ ALAB-318.2.1/index.js
@@ -5,6 +5,8 @@ const dvd = require('./dvd');
 
 const app = express();
 
+app.set('view engine', 'ejs'); // Set the view engine to EJS
+
 // Use Morgan middleware for logging
 app.use(morgan('dev'));
 
@@ -14,3 +16,4 @@ app.use('/dvd', dvd);
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
