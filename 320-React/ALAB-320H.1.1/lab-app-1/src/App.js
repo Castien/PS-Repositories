a06731b557e8aa26c3be33ee1b9f616e2d74c135
Eileen Lowers <funmaster22@hotmail.com>
2024-01-22 15:59:20 -0500

    adding React files and lab

---
+++ 320-React/ALAB-320H.1.1/lab-app-1/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import HomePage from "./components/HomePage.js"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my United States of Whatever.</h1>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ 320-React/ALAB-320H.1.1/lab-app-1/src/components/EmployeeListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EmployeeListItem() {
+    return (
+        <div>
+            <h2>EmployeeListItem</h2>
+        </div>
+    );
+};
+
+export default EmployeeListItem;
