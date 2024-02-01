c50cb09794e7b73bb19c42a19fca6d091a65cc8b
Eileen Lowers <funmaster22@hotmail.com>
2024-02-01 11:56:19 -0500

    first commit for ALAB-320H.8.1

--- 320-React/my_movies-project/frontend/src/App.js
+++ 320-React/my_movies-project/frontend/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import MoviesList from "./components/MoviesList";
+import MovieDetails from "./components/MovieDetails";
+import MainPage from "./pages/MainPage";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  // useEffect
+  useEffect(() => {
+    // connect to the backend
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:4000/api/movies");
+      const data = await res.json();
+      console.log(data);
+      // set the data to the state movies variable
+      setMovies(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movies FullStack App</h1>
+
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/movies" element={<MoviesList movies={movies} />} />
+        <Route path='/movies/:id' element={<MovieDetails />}/>
+      </Routes>
     </div>
   );
 }
